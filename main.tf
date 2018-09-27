fbd01502e8ab311a25d6213677ac9b9a0304234b
Lukas Loesche <lukas@mesosphere.io>
2018-09-27 16:21:12 +0200

    Add documentation header to main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,37 @@
+/**
+ * [![Build Status](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-gcp-instance/job/master/badge/icon)](https://jenkins-terraform.mesosphere.com/service/dcos-terraform-jenkins/job/dcos-terraform/job/terraform-gcp-instance/job/master/)
+ * # DC/OS Instances
+ *
+ * Creates generic instances for DC/OS nodes
+ *
+ * ## Usage
+ *
+ *```hcl
+ * module "masters" {
+ *   source  = "dcos-terraform/instance/gcp"
+ *   version = "~> 0.0"
+ *
+ *   providers = {
+ *     google = "google"
+ *   }
+ *
+ *   cluster_name             = "${var.cluster_name}"
+ *   hostname_format          = "${var.hostname_format}"
+ *   num                      = "${var.num_masters}"
+ *   image                    = "${coalesce(var.image, module.dcos-tested-oses.gcp_image_name)}"
+ *   user_data                = "${var.image == "" ? module.dcos-tested-oses.os-setup : var.gcp_user_data}"
+ *   machine_type             = "${var.machine_type}"
+ *   instance_subnetwork_name = ["${var.master_subnetwork_name}"]
+ *   ssh_user                 = "${var.ssh_user}"
+ *   public_ssh_key           = "${var.public_ssh_key}"
+ *   zone_list                = "${var.zone_list}"
+ *   disk_type                = "${var.disk_type}"
+ *   disk_size                = "${var.disk_size}"
+ *   tags                     = "${var.tags}"
+ * }
+ *```
+ */
+
 provider "google" {}
 
 data "google_client_config" "current" {}
