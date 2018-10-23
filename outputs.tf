ac7aa4ce88166f13ff672be48cc3d5e50ccab070
Weston Bassler <wbassler@mesosphere.com>
2018-10-23 12:47:58 -0400

    terraform fmt

--- outputs.tf
+++ outputs.tf
@@ -1,111 +1,92 @@
-
 output "num_instances" {
   description = "How many instances should be created"
   value       = "${var.num_instances}"
 }
-
 
 output "name_prefix" {
   description = "Cluster Name"
   value       = "${var.cluster_name}"
 }
 
-
 output "machine_type" {
   description = "Instance Type"
   value       = "${var.machine_type}"
 }
-
 
 output "dcos_version" {
   description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
   value       = "${var.dcos_version}"
 }
 
-
 output "dcos_instance_os" {
   description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
   value       = "${var.dcos_instance_os}"
 }
-
 
 output "zone_list" {
   description = "Element by zone list"
   value       = "${var.zone_list}"
 }
 
-
 output "image" {
   description = "Source image to boot from"
   value       = "${var.image}"
 }
-
 
 output "disk_type" {
   description = "Disk Type to Leverage"
   value       = "${var.disk_type}"
 }
 
-
 output "disk_size" {
   description = "Disk Size in GB"
   value       = "${var.disk_size}"
 }
-
 
 output "instance_subnetwork_name" {
   description = "Instance Subnetwork Name"
   value       = "${var.instance_subnetwork_name}"
 }
 
-
 output "user_data" {
   description = "User data to be used on these instances (cloud-init)"
   value       = "${var.user_data}"
 }
-
 
 output "ssh_user" {
   description = "SSH User"
   value       = "${coalesce(var.ssh_user, module.dcos-tested-oses.user)}"
 }
 
-
 output "public_ssh_key" {
   description = "SSH Public Key"
   value       = "${var.public_ssh_key}"
 }
-
 
 output "allow_stopping_for_update" {
   description = "If true, allows Terraform to stop the instance to update its properties"
   value       = "${var.allow_stopping_for_update}"
 }
 
-
 output "private_ips" {
   description = "List of private ip addresses created by this module"
   value       = ["${google_compute_instance.instances.*.network_interface.0.address}"]
 }
-
 
 output "public_ips" {
   description = "List of public ip addresses created by this module"
   value       = ["${google_compute_instance.instances.*.network_interface.0.access_config.0.assigned_nat_ip}"]
 }
 
-
 output "instances_self_link" {
   description = "List of instance self links"
   value       = ["${google_compute_instance.instances.*.self_link}"]
 }
 
-
 output "scheduling_preemptible" {
   description = "Deploy instance with preemptible scheduling. (bool)"
   value       = "${var.scheduling_preemptible}"
 }
-
 
 output "prereq_id" {
   description = "Prereq id used for dependency"
--- variables.tf
+++ variables.tf
@@ -2,26 +2,21 @@ variable "num_instances" {
   description = "How many instances should be created"
 }
 
-
 variable "cluster_name" {
   description = "Name of the DC/OS cluster"
 }
-
 
 variable "machine_type" {
   description = "Instance Type"
 }
 
-
 variable "dcos_version" {
   description = "Specifies which DC/OS version instruction to use. Options: 1.9.0, 1.8.8, etc. See dcos_download_path or dcos_version tree for a full list."
 }
 
-
 variable "dcos_instance_os" {
   description = "Operating system to use. Instead of using your own AMI you could use a provided OS."
 }
-
 
 variable "ssh_private_key_filename" {
   description = "Path to the SSH private key"
@@ -31,37 +26,30 @@ variable "ssh_private_key_filename" {
   default = "/dev/null"
 }
 
-
 variable "zone_list" {
   description = "Element by zone list"
   type        = "list"
 }
 
-
 variable "image" {
   description = "Source image to boot from"
 }
-
 
 variable "disk_type" {
   description = "Disk Type to Leverage"
 }
 
-
 variable "disk_size" {
   description = "Disk Size in GB"
 }
-
 
 variable "instance_subnetwork_name" {
   description = "Instance Subnetwork Name"
 }
 
-
 variable "user_data" {
   description = "User data to be used on these instances (cloud-init)"
 }
-
 
 variable "ssh_user" {
   description = "SSH User"
@@ -72,12 +60,10 @@ variable "public_ssh_key" {
   description = "SSH Public Key"
 }
 
-
 variable "allow_stopping_for_update" {
   description = "If true, allows Terraform to stop the instance to update its properties"
   default     = "true"
 }
-
 
 variable "tags" {
   type        = "list"
@@ -91,12 +77,10 @@ variable "labels" {
   default     = {}
 }
 
-
 variable "hostname_format" {
   description = "Format the hostname inputs are index+1, region, cluster_name"
   default     = "%[3]s-instance%[1]d-%[2]s"
 }
-
 
 variable "scheduling_preemptible" {
   description = "Deploy instance with preemptible scheduling. (bool)"
